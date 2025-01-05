ad8ad4844c8f2d1d49bcb68b8be3d9b8d087682e
ohjinhokor <ohjinho7@gmail.com>
2025-01-05 19:24:42 +0900

    feat: start project & 멀티 모듈 세팅

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+rootProject.name = "the-great-escape"
+
+include("presentation")
+include("application")
+include("domain")
+include("infra")
