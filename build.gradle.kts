b835ea7d2d33588bae06e89079714d6bb665a6c0
ohjinhokor <ohjinho7@gmail.com>
2025-02-06 17:43:49 +0900

    chore: root gradle 파일 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,17 +5,6 @@ plugins {
 group = "board-game"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
     jvmToolchain(21)
 }
